430a4a06d61ad08351ea4b9809aeb4b5fcede455
liuzujun <liuzujun@medlinker.com>
2020-10-23 17:42:30 +0800

    up

--- txt_to_execl.py
+++ txt_to_execl.py
@@ -1,23 +1,5 @@
-import pandas as pd
+# import pandas as pd
 import xlwt
-
-# def deal_file(file_path):
-#     temp = pd.read_csv(file_path, sep='|')
-#     return temp.iloc[0:temp.shape[0]]
-# 
-# 
-# if __name__ == "__main__":
-#     file_name = "save-202009"
-#     data = deal_file(file_name + ".txt")
-# 
-#     data["订单编号"] = data["订单编号"].fillna(-1)
-#     data["订单编号"] = data["订单编号"].astype("int64")
-# 
-#     data["药品订单号"] = data["药品订单号"].fillna(-1)
-#     data["药品订单号"] = data["药品订单号"].astype("int64")
-# 
-#     data.to_excel(file_name + ".xlsx", index=False)
-
 
 if __name__ == "__main__":
     file_name = "save-202009"
@@ -28,6 +10,8 @@ if __name__ == "__main__":
     row = 0
     for line in file:
         data = line.split("|")
+        if len(data) == 11:
+            data.insert(7, "-")
         for col in range(len(data)):
             if data[col] == "":
                 data[col] = "0"
