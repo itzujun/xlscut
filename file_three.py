7d6f9702a1caf61f5c1a6835edd3a1e5e254a3cd
liuzujun <liuzujun@medlinker.com>
2021-09-06 17:52:28 +0800

    up

--- file_three.py
+++ file_three.py
@@ -1,12 +1,26 @@
 # coding=utf-8
 
+import time
+
 if __name__ == "__main__":
+    begin_time = time.time()
     file_name = "save-202108"
     file = open(file_name + ".txt")
     temp = ""
     for line in file:
-        line = line.replace("|", ",")
-        temp = temp + line
+        li = line.split("|")
+        if str(li[2]).__contains__("2"):
+            li[2] = "'" + li[2]
+        if str(li[3]).__contains__("2"):
+            li[3] = "'" + li[3]
+        if str(li[4]).__contains__("2"):
+            li[4] = "'" + li[4]
+        if str(li[5]).__contains__("2"):
+            li[5] = "'" + li[5]
+        s = ",".join(li)
+        temp = temp + s
     f = open("./files/" + file_name + ".csv", 'w')
     f.write(temp)
     f.close()
+    end_time = time.time()
+    print("耗时： {}(秒)".format(round(end_time - begin_time, 2)))
