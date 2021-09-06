9ede983ceaa96e16b4c414d3e4e64c3a60b252d7
liuzujun <liuzujun@medlinker.com>
2021-09-06 17:15:26 +0800

    up

--- file_two.py
+++ file_two.py
@@ -4,7 +4,7 @@ import time
 
 if __name__ == "__main__":
     begin_time = time.time()
-    file_name = "save-allinpay-202107"
+    file_name = "save-202108"
     file = open(file_name + ".txt")
     workbook = openpyxl.Workbook()
     book_sheet = workbook.create_sheet(index=0)
@@ -14,6 +14,9 @@ if __name__ == "__main__":
         for col in range(0, len(data)):
             if data[col] == "":
                 data[col] = "0"
+            value = data[col]
+            if str(data[col]).__contains__("\n"):
+                value.replace("\n", "")
             book_sheet.cell(row, col + 1, data[col])
         row = row + 1
     workbook.save("./files/" + file_name + ".xlsx")
